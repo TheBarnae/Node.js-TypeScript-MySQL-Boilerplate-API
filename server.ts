06891e4257c4af69e966e2a05eb21300a7212a28
barnae <xyeahgirlx@gmail.com>
2026-04-16 03:03:11 +0800

    finishhed na. few  more testings nlng

--- server.ts
+++ server.ts
@@ -25,5 +25,5 @@ app.use('/api-docs', swaggerDocs);
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3306;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 app.listen(port, () => console.log('Server listening on port ' + port));
